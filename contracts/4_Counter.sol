4beca88c4be5df08b136f487bdd7c2e6ae82ccf8
parthsolanki1498 <parthsolanki1498@gmail.com>
2023-06-25 04:05:00 -0400

    Counter Smart Contract Commit

---
+++ contracts/4_Counter.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Counter {
+    uint public count = 0;
+
+    function increment() public returns (uint){
+        count+=1;
+        return count;
+    }
+}
